cd3056e55ee61cf528e9998972318924f18a7ced
Christopher Bertels <bakkdoor@flasht.de>
2009-10-23 04:39:51 +0200

    added command_to_bin helper function & some test code.

--- src/stupidvm.c
+++ src/stupidvm.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "opcodes.h"
 #include "stack.h"
@@ -14,39 +15,64 @@ REGISTER regs[16] = {0,0,0,0,
                      0,0,0,0,};
 
 /* 8-bit programm counter */
-REGISTER PC; 
+int PC; 
 
 
 COMMAND* create_programm(void)
 {
-  COMMAND* p = malloc(10 * sizeof(COMMAND));
-  int i;
-  
-  for(i=0; i<10; i++) {
-    p[i] = i;
-  }
-
+  COMMAND* p = malloc(1 * sizeof(COMMAND));
+  /* int i; */
+  /* for(i=0; i<9; i++) { */
+  /*   p[i] = build_cmd(HALT, 1, 2); */
+  /* } */
+  p[0] = build_cmd(HALT, 2, 3);
   return p;
 }
+
+void command_to_bin(COMMAND dez,char a[])
+{
+   char zw[33]; long r;
+
+   a[0]='\0';
+   while(dez>0)
+   {
+      r=dez%2;
+      if(r==1) strcpy(zw,"1");
+      else strcpy(zw,"0");
+      strcat(zw,a);
+      strcpy(a,zw);
+      dez/=2;
+   }
+} 
 
 
 void run(COMMAND* programm)
 {
   COMMAND cmd;
-  OPCODE opc;
-  Stack* stack;
-  init(stack);
+  OPERAND opc;
+  Stack stack;
+  init(&stack);
   PC = 0;
-    
-  while(1) {
-    cmd = programm[PC++];
-    
+  char str[16];
+  command_to_bin(programm[PC], &str);
+
+  printf("command is: %s\n", str);
+  
+  /*  while(1) { */
+    cmd = programm[PC];
     opc = opcode(cmd);
-    
-    if(opc == POP) {
-      pop(stack);
+    command_to_bin(opc, &str);
+    printf("opcode is: %s\n", str);
+
+    switch(opc) {
+    case POP:
+      pop(&stack);
+      break;
+    case 19:
+      printf("halting machine\n");
+      return;
     }
-  }
+  /* } */
 }
 
 int main(int argc, char *argv[])
