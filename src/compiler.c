554786a817dd52f2236bef3fe01bc7274d50e391
Christopher Bertels <bakkdoor@flasht.de>
2009-10-27 19:40:12 +0100

    started work on a somewhat more "real" compiler. i guess assembler is the better word..

--- src/compiler.c
+++ src/compiler.c
@@ -13,9 +13,20 @@
 
     You should have received a copy of the GNU General Public License
     along with stupidvm.  If not, see <http://www.gnu.org/licenses/>.
- */
+*/
 
 #include "includes.h"
+
+void usage(void) {
+  fprintf(stderr, "Usage:\n");
+  fprintf(stderr, "./compiler source_file.sasm output_file.stupid\n");
+}
+
+
+Instruction instruction_from_line(char *line) {
+  Instruction i;
+  return i;
+}
 
 /**
    This is a very simple implementation of a bytecode compiler.
@@ -23,16 +34,38 @@
    examples.h with a macro) and write them binary to a specified file.
  */
 int main(int argc, char *argv[]) {
-  if(argc > 1) {
-    char* filename = argv[1];
-    Instruction p[] = procedures();
-    int length = sizeof(p) / sizeof(Instruction);
-    FILE *file = fopen(filename, "w+b");
-    fwrite(p, sizeof(Instruction), length, file);
-    fclose(file);
+  if(argc == 3) {
+    char *source_file = argv[1];
+    char *dest_file = argv[2];
+    char *line;
+    Instruction inst;
+
+    FILE *source = fopen(source_file, "r");
+    FILE *destination = fopen(dest_file, "w+b");
+
+    /* check, if opening worked */
+    if(!source) {
+      fprintf(stderr, "ERROR with opening file: %s", source_file);
+    }
+    if(!destination) {
+      fprintf(stderr, "ERROR with opening file: %s", dest_file);
+    }
+
+    /* compile line & write to destination file */
+    while( (line = read_line(source)) ) {
+      inst = instruction_from_line(line);
+      printf("writing instruction. opc: %d, op1: %d, op2: %d\n", inst.opcode, inst.op1, inst.op2);
+      fwrite(&inst, sizeof(Instruction), sizeof(Instruction), destination);
+      free(line);
+    }
+
+    fclose(source);
+    fclose(destination);
+
     return 0;
   } else {
     fprintf(stderr, "Error: Please specify an output filename to save bytcode to.\n");
+    usage();
     return -1;
   }
 }
