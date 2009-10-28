4c99a67312c588d618b91428c39b1782fed82509
Christopher Bertels <bakkdoor@flasht.de>
2009-10-28 02:12:54 +0100

    more work on compiler. works somewhat unfortunately stupidvm can't run the exported binary files yet.

--- src/compiler.c
+++ src/compiler.c
@@ -22,15 +22,48 @@ void usage(void) {
   fprintf(stderr, "./compiler source_file.sasm output_file.stupid\n");
 }
 
+/**
+   returns 3-element array of string containing the values of:
+   - opcode (instruction command name)
+   - operand 1 (or 0, if not given)
+   - operand 2 (or 0, if not given)
+*/
+char** instruction_words(char *line) {
+  char **words;
+  char *pch;
+  int i = 0;
+  
+  words = malloc(sizeof(char*) * 3);
+
+  pch = strtok(line, " ");
+  while(pch != NULL) {
+    words[i] = pch;
+    pch = strtok(NULL, ", ");
+    i++;
+  }
+  
+  /* fill up any missing operands with 0 */
+  for(; i < 3; i++) {
+    words[i] = (char)0;
+  }
+
+  return words;
+}
 
 Instruction instruction_from_line(char *line) {
-  Instruction i;
-  return i;
+  Instruction ins;
+  char **words = instruction_words(line);  
+
+  ins.opcode = opcode_from_string(words[0]);
+  ins.op1 = words[1] ? (Operand)atoi(words[1]) : (Operand)0;
+  ins.op2 = words[2] ? (Operand)atoi(words[2]) : (Operand)0;
+
+  return ins;
 }
 
 FILE* exec_cpp(char *source_filename) {
   char command[500] = {"cpp "};
-  strcat(command, source_file);
+  strcat(command, source_filename);
   return popen(command, "r");
 }
 
@@ -44,8 +77,10 @@ int main(int argc, char *argv[]) {
     char *source_file = argv[1];
     char *dest_file = argv[2];
     char *line;
-    Instruction inst;
+    Instruction* inst;
+    int inst_size;
     FILE *source, *destination;
+    int i = 0;
 
     source = exec_cpp(source_file);
     destination = fopen(dest_file, "w+b");
@@ -58,18 +93,31 @@ int main(int argc, char *argv[]) {
       fprintf(stderr, "ERROR with opening file: %s", dest_file);
     }
 
+    /* by default, we start with a size of 1000 instructions */
+    inst_size = 1000;
+    inst = malloc(sizeof(Instruction) * inst_size);
+    
     /* compile line & write to destination file */
-    while( (line = read_line(source)) ) {      
+    while( (line = read_line(source)) ) {
+      /* check counter and realloac inst if necessary*/
+      if(i == inst_size) {
+        inst_size *= 2;
+        inst = realloc(inst, inst_size);
+      }
+      
       /* only write lines without a '#' in it (these lines are added
          by cpp and are ignored) */
-      if(!strchr(line, '#')) {
-          printf("line is: %s\n", line);
-        inst = instruction_from_line(line);
-        printf("writing instruction. opc: %d, op1: %d, op2: %d\n", inst.opcode, inst.op1, inst.op2);
-        fwrite(&inst, sizeof(Instruction), sizeof(Instruction), destination);
+      if(!strchr(line, '#') && !empty_string(line)) {
+        printf("line is: %s\n", line);
+        inst[i] = instruction_from_line(line);
+        printf("writing instruction. opc: %d, op1: %d, op2: %d\n", inst[i].opcode, inst[i].op1, inst[i].op2);
       }
       free(line);
+      i++; /* counter +1 */
     }
+
+    /* finally, write all instructions to file */
+    fwrite(inst, sizeof(Instruction), sizeof(Instruction), destination);
 
     fclose(source);
     fclose(destination);
